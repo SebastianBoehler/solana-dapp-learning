5614200c9157ff54cd3b5907689c5fbdc9a59639
Sebastian Boehler <basti.boehler@hotmail.de>
2023-12-26 16:19:42 +0700

    fix: UI changes to presentate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { SendSOLToRandomAddress } from '@/components/sendLamports';
-import { HelloWorld } from '@/components/counterProgram';
+import { Counter } from '@/components/counter';
+import './style.css'
 
 const IndexPage: React.FC = () => {
+
     return (
         <div>
-            <h1>Welcome to my Solana Dapp Learning!</h1>
-            <WalletMultiButton />
-            <HelloWorld />
+            <div className="flex justify-end p-4">
+                <div className="ml-4">
+                    <WalletMultiButton style={{ backgroundColor: 'lightblue' }} />
+                </div>
+            </div>
+            <Counter />
         </div>
     );
 };
